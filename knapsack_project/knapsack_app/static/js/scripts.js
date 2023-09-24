9e9a1186c056899dcaca69f558cd185f5e9affd9
hlin863 <zcabhli@ucl.ac.uk>
2023-09-24 23:09:18 +0100

    EXAMPLE 1 COMPLETE

--- knapsack_project/knapsack_app/static/js/scripts.js
+++ knapsack_project/knapsack_app/static/js/scripts.js
@@ -21,8 +21,8 @@ document.getElementById("calculateKnapsack").addEventListener('click', function 
     const quantityInput = document.getElementById('quantity').value || 'N/A';
     const weightInput = document.getElementById('weight').value || 'N/A';
 
-    const capacity = 10;
-    const n = 4;
+    const capacity = 850; // a constant for the knapsack capacity.
+    const n = 50; // determines number of items in weights and values. 
 
     // define values as a list from 0 to valueInput in intervals of 5s.
     // const values = [];
@@ -49,8 +49,21 @@ document.getElementById("calculateKnapsack").addEventListener('click', function 
     // const weights = [5, 10, 15, 20];
     // const quantities = [1, 2, 3, 4];
 
-    const values = [1680, 1440, 1840];
-    const weights = [0.265, 0.5, 0.441];
+    // const values = [1680, 1440, 1840];
+    // const weights = [0.265, 0.5, 0.441];
+    const values = [
+        360, 83, 59, 130, 431, 67, 230, 52, 93, 125, 670, 892, 600, 38, 48, 147,
+        78, 256, 63, 17, 120, 164, 432, 35, 92, 110, 22, 42, 50, 323, 514, 28,
+        87, 73, 78, 15, 26, 78, 210, 36, 85, 189, 274, 43, 33, 10, 19, 389, 276,
+        312
+    ]; 
+    // logs values size
+    console.log(values.length);
+    const weights = [
+        7, 0, 30, 22, 80, 94, 11, 81, 70, 64, 59, 18, 0, 36, 3, 8, 15, 42, 9, 0,
+        42, 47, 52, 32, 26, 48, 55, 6, 29, 84, 2, 4, 18, 56, 7, 29, 93, 44, 71,
+        3, 86, 66, 31, 65, 0, 79, 20, 65, 52, 13
+    ];
     const quantities = [0.41, 0.13, 0.29];
     const result = knapsack(values, weights, capacity, n);
 
