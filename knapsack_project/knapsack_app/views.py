2721ff548a4f5b402286f8bc84d43349039390ef
hlin863 <zcabhli@ucl.ac.uk>
2023-09-24 17:48:36 +0100

    Demo Front End inventory

--- knapsack_project/knapsack_app/views.py
+++ knapsack_project/knapsack_app/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render, redirect
 from .forms import ItemForm
 from .models import Item
 
+from django.http import JsonResponse
+
+
+def ingredient_library(request):
+    product_data = {
+        'flour': {
+            'value': '$2.99',
+            'quantity': '500g',
+            'weight': '0.5 kg',
+        },
+        'butter': {
+            'value': '$3.49',
+            'quantity': '200g',
+            'weight': '0.2 kg',
+        },
+        'sugar': {
+            'value': '$1.99',
+            'quantity': '1 kg',
+            'weight': '1 kg',
+        },
+    }
+    return render(request, 'index.html', {'product_data': product_data})
+
 
 def index(request):
     if request.method == 'POST':
