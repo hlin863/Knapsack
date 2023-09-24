5245959c0f15efcbd6885e445eddf33a0528b509
hlin863 <zcabhli@ucl.ac.uk>
2023-09-24 17:52:32 +0100

    Updated an entry
    Experimented with updating inventory data

--- knapsack_project/knapsack_app/views.py
+++ knapsack_project/knapsack_app/views.py
@@ -2,7 +2,41 @@ from django.shortcuts import render, redirect
 from .forms import ItemForm
 from .models import Item
 
+# import JSONResponse
 from django.http import JsonResponse
+
+
+def update_inventory(request):
+    if request.method == 'POST':
+        # Retrieve the updated values from the POST request
+        product_name = request.POST.get('product')
+        value = request.POST.get('value')
+        quantity = request.POST.get('quantity')
+        weight = request.POST.get('weight')
+
+        # You can update the inventory data in your database here
+        # For this example, let's assume you have a model named "InventoryItem"
+
+        # First, you need to retrieve the item from the database, possibly by an ID or some unique identifier
+        # For demonstration purposes, we assume you have an item with ID 1
+        item_id = 1
+        try:
+            item = Item.objects.get(pk=item_id)
+            # Update the item's attributes with the new values
+            item.product_name = product_name
+            item.value = value
+            item.quantity = quantity
+            item.weight = weight
+            item.save()
+        except Item.DoesNotExist:
+            # Handle the case where the item doesn't exist
+            pass
+
+        # Return a JSON response indicating success
+        return JsonResponse({'message': 'Inventory details updated successfully'})
+
+    # Handle GET requests or other methods
+    return JsonResponse({'message': 'Invalid request method'})
 
 
 def ingredient_library(request):
--- knapsack_project/knapsack_project/urls.py
+++ knapsack_project/knapsack_project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from knapsack_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('knapsack_app.urls')),
+    # Add the URL pattern for the update_inventory view
+    path('update_inventory/', views.update_inventory, name='update_inventory'),
 ]
